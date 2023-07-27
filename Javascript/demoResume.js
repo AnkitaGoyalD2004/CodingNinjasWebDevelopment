cfefe1e9903f3eaec2f6a9b0e3270a8f846c38e3
AnkitaGoyalD2004 <117336913+AnkitaGoyalD2004@users.noreply.github.com>
2023-07-27 22:44:40 +0530

    Update demoResume.js

--- Javascript/demoResume.js
+++ Javascript/demoResume.js
@@ -1,4 +1,34 @@
-var scrollInterval = setInterval(function(){
-    window.scrollBy(0,50);
-} ,50);
-clearInterval(scrollInterval);
+
+// var currentPos = 0;
+// var targetPos = 400;
+// var smoothScroll = setInterval(fn, 50);
+
+// function fn() {
+//     if (currentPos >= targetPos) {
+//         clearInterval(smoothScroll);
+//     }
+//    currentPos += 50;
+//     window.scrollBy(0, 50);
+// }
+
+var educationSection = document.getElementById('education');
+var coordinates = educationSection.getBoundingClientRect();
+
+
+var anchorTags = document.querySelector('.nav-menu a');
+for( i = 0 ; i < anchorTags.clientHeight; i ++){
+    anchorTags[i].addEventListener('click' , function(event){
+        event.preventDefault();
+        var targetSectionID = this.textContent.trim().toLowercase();
+        var targetSection  = document.getElementById(targetSectionID);
+        console.log(targetSectionID);
+        var targetSectionCoordinates = target.getBoundingClientRect();
+        var Interval = setInterval(function(){
+            if(targetSectionCoordinates.top <= 0){
+                clearInterval(interval);
+                return;
+            }
+            window.scrollBy(0,50);
+        }, 50);
+    });
+}
