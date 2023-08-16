d044b65a0cc957355245515ab171f42c4adfeaeb
AnkitaGoyalD2004 <117336913+AnkitaGoyalD2004@users.noreply.github.com>
2023-08-16 22:06:55 +0530

    Add files via upload

---
+++ Node/Module/Operations.js
@@ -0,0 +1,6 @@
+module.exports.add = function(a,b){ // in this we use module.exports so that we can access this outside the file
+    return a + b;
+}
+module.exports.multiply = function(a,b){
+    return a*b;
+}
