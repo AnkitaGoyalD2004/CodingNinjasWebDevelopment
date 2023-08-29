6381ab736cad0c7bd7d4e39a275f06c9dadd871c
AnkitaGoyalD2004 <117336913+AnkitaGoyalD2004@users.noreply.github.com>
2023-08-29 22:36:13 +0530

    Add files via upload

---
+++ Javascript/Constructors & Prototypes/Object.js
@@ -0,0 +1,7 @@
+// creating an object
+var obj = {};
+var obj = new Object();// Object works as a function here
+// for checking the property exist or not 
+Object.hasOwnProperty('color');
+// Object is a constructor function
+// object this is a non primitive data type basically this is a key value pair
